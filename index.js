01c208e7d24ff8dae99020ef1768780a619f4111
Bruno PC <blgianini@gmail.com>
2022-11-03 13:55:50 -0300

    cron brasileiro A

--- index.js
+++ index.js
@@ -56,6 +56,29 @@ var teamsNBA = [
   {id: 37, name: 'San Antonio Spurs'}
 ]
 
+var teamsBrasileiro = [
+  {id: 4, name: 'Flamengo'},
+  {id: 3, name: 'Corinthians'},
+  {id: 89, name: 'Palmeiras'},
+  {id: 90, name: 'Internacional'},
+  {id: 91, name: 'Fluminense'},
+  {id: 92 ,name: 'Atletico Paranaense'},
+  {id: 93, name: 'Fortaleza'},
+  {id: 94, name: 'Atletico Mineiro'},
+  {id: 95, name: 'Sao Paulo'},
+  {id: 96, name: 'Botafogo'},
+  {id: 97, name: 'América Mineiro'},
+  {id: 98, name: 'Santos'},
+  {id: 99, name: 'Goiás'},
+  {id: 100, name: 'Bragantino-SP'},
+  {id: 101, name: 'Coritiba'},
+  {id: 102, name: 'Ceará'},
+  {id: 103, name: 'Atletico Goianiense'},
+  {id: 104, name: 'Cuiabá'},
+  {id: 105, name: 'Avai'},
+  {id: 106, name: 'Juventude'}
+]
+
 function get_nba_matches(){
     axios.get(`https://api.the-odds-api.com/v4/sports/basketball_nba/odds`, {
         params: {
@@ -204,5 +227,155 @@ function get_nba_matches(){
 
 }
 
+function get_seriaa_matches(){
+  axios.get(`https://api.the-odds-api.com/v4/sports/soccer_brazil_campeonato/odds`, {
+    params: {
+        apiKey,
+        regions,
+        markets,
+        oddsFormat,
+        dateFormat,
+    }
+})
+
+  .then(response => {
+  
+      var con = mysql.createConnection({
+          host: "162.214.49.127",
+          user: "pokermob_better",
+          password: "Admlock22#",
+          database: "pokermob_bet"
+      });
+  
+    con.connect(function(err) { 
+      if (err) throw err;
+      console.log("Connected!");
+    
+      for(let i = 0; i < response.data.length; i++){
+        team1_id = teamsBrasileiro.find(item => item.name === response.data[i].home_team)
+        team2_id = teamsBrasileiro.find(item => item.name === response.data[i].away_team)
+        let start_date = response.data[i].commence_time
+
+        let date = new Date(start_date)
+        let date_certo = new Date(start_date)
+        let tomorrow = new Date();
+        date_certo.setTime(date_certo.getTime() - 10*60000)
+        tomorrow.setDate(date.getDate()+1)
+        date.setHours(-3,0,0,0)
+        tomorrow.setHours(-3,0,0,0)
+        date_certo = date_certo.toString().split('G')[0]
+  
+        console.log(tomorrow.toISOString())
+
+        let odd_time_1 = response.data[i].bookmakers[0].markets[0].outcomes[0].price
+        let odd_time_2 = response.data[i].bookmakers[0].markets[0].outcomes[1].price
+        searchResults(team1_id.name + ' VS ' + team2_id.name + ' / ' + date_certo, team1_id, team2_id, start_date, id, date, date_certo, tomorrow,odd_time_1,odd_time_2).then(() => {
+          
+       });
+      }
+    });
+    console.log('Ciclo terminado')
+    con.end()
+  })
+
+  async function searchResults(name, team1_id, team2_id, start_date, id, date, date_certo, tomorrow,odd_time_1,odd_time_2){
+
+      var select = mysql.createConnection({
+          host: "162.214.49.127",
+          user: "pokermob_better",
+          password: "Admlock22#",
+          database: "pokermob_bet"
+        });
+  
+          var sql = "SELECT * FROM `game_matches` WHERE `name` = '"+ name +"'";
+  
+          await select.query(sql, function (err, result) {
+              if (err) throw err;
+              if(result.length == 0){
+                  insertMatch(team1_id, team2_id, start_date, id, date, date_certo, tomorrow,odd_time_1,odd_time_2).then(() =>{
+                  })
+              }else{
+                  updateOdds(result[0].id,odd_time_1,odd_time_2)
+              }
+          })
+      select.end()
+  }
+  
+  async function updateOdds(id,odd_time_1,odd_time_2){
+      var update = mysql.createConnection({
+          host: "162.214.49.127",
+          user: "pokermob_better",
+          password: "Admlock22#",
+          database: "pokermob_bet"
+        });
+  
+        var sql = "SELECT * FROM `game_questions` WHERE `match_id` = "+id+"";
+  
+        await update.query(sql, async function (err, result) {
+          if (err) throw err;
+  
+          var sql = "SELECT * FROM `game_options` WHERE`question_id` = '"+ result[0].id +"'"
+  
+          await update.query(sql, async function (err, result) {
+              if (err) throw err;
+  
+              var sql = "UPDATE `game_options` SET `ratio`= '"+odd_time_1+"' WHERE `id` = '"+result[0].id+"'"
+              var sql2 = "UPDATE `game_options` SET `ratio`= '"+odd_time_2+"' WHERE `id` = '"+result[1].id+"'"
+              
+              await update.query(sql, function (err, result) {
+                  if (err) throw err;
+              })
+  
+              await update.query(sql2, function (err, result) {
+                  if (err) throw err;
+                  update.end()
+              })
+  
+          })
+  
+      })
+  }
+  
+  async function insertMatch(team1_id, team2_id, start_date, id, date, date_certo, tomorrow,odd_time_1,odd_time_2){
+  
+      var insert = mysql.createConnection({
+          host: "162.214.49.127",
+          user: "pokermob_better",
+          password: "Admlock22#",
+          database: "pokermob_bet"
+        });
+  
+        var sql = "INSERT INTO game_matches (id, category_id, tournament_id, team1_id, team2_id, name, start_date, end_date, status, is_unlock) VALUES (id, 1, 16, "+ team1_id.id +", "+ team2_id.id +", '"+ team1_id.name +" VS "+ team2_id.name +" / "+ date_certo +"', '"+ date.toISOString() +"', '"+ tomorrow.toISOString() +"', 1, 0)";
+  
+          await insert.query(sql, async function (err, result) {
+              if (err) throw err;
+              let match_id = result.insertId;
+  
+              var sql = "INSERT INTO game_questions (`match_id`,`result_id`,`name`,`status`,`is_unlock`,`result`,`limit`,`creator_id`,`end_time`) VALUES ("+ match_id +", null, 'Quem Ganha', 1, 0, 0, 100, 1, '"+ tomorrow.toISOString() +"')";
+          
+              await insert.query(sql, async function (err, result) {
+                if (err) throw err;
+                let question_id = result.insertId;
+      
+                var sql = "INSERT INTO `game_options`(`match_id`, `question_id`, `creator_id`, `option_name`, `invest_amount`, `return_amount`, `minimum_amount`, `ratio`, `status`) VALUES ("+ match_id +", "+ question_id +", 1, '"+ team1_id.name +"', 0, 0, 2, "+ odd_time_1 +", 1)";
+                var sql2 = "INSERT INTO `game_options`(`match_id`, `question_id`, `creator_id`, `option_name`, `invest_amount`, `return_amount`, `minimum_amount`, `ratio`, `status`) VALUES ("+ match_id +", "+ question_id +", 1, '"+ team2_id.name +"', 0, 0, 2, "+ odd_time_2 +", 1)";
+      
+                await insert.query(sql, function (err, result) {
+                  if (err) throw err;
+                })
+      
+                await insert.query(sql2, function (err, result) {
+                  if (err) throw err;
+                  insert.end()
+                })
+                
+                
+              })
+          })
+          
+  }
+
+}
 
 cron.schedule('0 1 * * *', () => {get_nba_matches()})
+cron.schedule('1 1 * * *', () => {get_seriaa_matches()})
--- insert_brasileiro_matches.js
+++ insert_brasileiro_matches.js
@@ -112,3 +112,7 @@ axios.get(`https://api.the-odds-api.com/v4/sports/soccer_brazil_campeonato/odds`
   });
 
 })
+
+
+
+
