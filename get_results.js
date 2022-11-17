aeb1e141c5253e59cae9668af597273231a8f351
Bruno Luiza <skaailpvp@gail.com>
2022-11-17 20:45:42 -0300

    Revert "resultado cadastrado text"
    This reverts commit 25ad4ce053f24fa636276d073d5213c79794931a.

--- get_results.js
+++ get_results.js
@@ -52,12 +52,12 @@ axios.get('https://api.the-odds-api.com/v4/sports/basketball_nba/scores/?daysFro
             
             con.query(sql, function (err, result) {
               if (err) throw err;
-              console.log(result.affectedRows + " resultado cadastrado");
+              console.log(result.affectedRows + " record(s) updated");
             });
 
             con.query(sql2, function (err, result) {
               if (err) throw err;
-              console.log(result.affectedRows + " resultado cadastrado");
+              console.log(result.affectedRows + " record(s) updated");
             });
 
         } catch (error) {
